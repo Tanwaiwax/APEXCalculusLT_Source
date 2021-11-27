faf4063e5c391cbb63679e3da29d8e85f3a550e3
Tim Prescott <teepeemm@gmail.com>
2021-11-26 21:43:31 -0600

    ready for epub?

--- slurm-apex.sh
+++ slurm-apex.sh
@@ -52,7 +52,7 @@ latexmlscripts="$HOME/.cpan/sources/authors/id/B/BR/BRMILLER/LaTeXML-0.8.6/blib/
 singularitydir="$HOME/latexml"
 printf '\\newcommand{\\thetitle}{Calculus}\n\\printincolor\n\\usethreeDgraphics\n\\renewcommand{\\monthYear}{June 2021}\n' > options.tex
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexml --quiet --destination=$base.xml --nocomments $base
+#singularity exec $singularitydir/latexml.sif $latexmlscripts/latexml --quiet --destination=$base.xml --nocomments $base
 
 exit_code=$?
 
@@ -80,7 +80,7 @@ fi
 
 tar czf web.tar.gz web/
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --split --destination=epub/index.html --stylesheet=apexepub.xsl $base.xml
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --split --destination=epub/index.epub --stylesheet=apexepub.xsl $base.xml
 
 exit_code=$?
 

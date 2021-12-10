1708eb3868926870b05f3fd27bf0dfe1d3b43f51
Tim Prescott <teepeemm@gmail.com>
2021-12-10 11:10:01 -0600

    test epub

--- slurm-alone.sh
+++ slurm-alone.sh
@@ -65,7 +65,7 @@ if [ "$exit_code" -ne "0" ]; then
     exit "$exit_code"
 fi
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --split --destination=standaloneweb/index.html --javascript=LaTeXML-maybeMathJax.js -css LaTeXML-marginpar.css $base.xml
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --split --destination=standaloneweb/index.html --javascript=LaTeXML-maybeMathJax.js -css=LaTeXML-marginpar.css $base.xml
 
 exit_code=$?
 
@@ -79,6 +79,8 @@ if [ "$exit_code" -ne "0" ]; then
 fi
 
 singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=standaloneweb/epub.xhtml --stylesheet=apexepub.xsl $base.xml
+
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=standaloneepub/index.epub $base.xml
 
 exit_code=$?
 

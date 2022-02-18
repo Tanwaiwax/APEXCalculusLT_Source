44c73e216ad232e097b64a6982478e53605c3952
Tim Prescott <teepeemm@gmail.com>
2022-02-18 11:20:38 -0600

    check compilation options

--- slurm-alone.sh
+++ slurm-alone.sh
@@ -55,9 +55,13 @@ printf '\\newcommand{\\thetitle}{Calculus}\n\\printincolor\n\\usethreeDgraphics\
 
 singularity exec $singularitydir/latexml.sif $latexmlscripts/latexml --destination=lab/moverMunder/moverMunder.xml --nocomments lab/moverMunder/moverMunder
 
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlc --destination=lab/moverMunder/moverMunder.xhtml --nocomments lab/moverMunder/moverMunder
+
 singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.html lab/moverMunder/moverMunder.xml
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.xhtml --stylesheet=apexepub.xsl lab/moverMunder/moverMunder.xml
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.xhtml lab/moverMunder/moverMunder.xml
+
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunderS.xhtml --stylesheet=apexepub.xsl lab/moverMunder/moverMunder.xml
 
 #singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.epub lab/moverMunder/moverMunder.xml
 

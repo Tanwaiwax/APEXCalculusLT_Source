dec2b9cf4daff3007e6f180bd02be7ac10a0d052
Tim Prescott <teepeemm@gmail.com>
2022-02-18 10:23:29 -0600

    recompiling latexmlpost

--- slurm-alone.sh
+++ slurm-alone.sh
@@ -53,11 +53,11 @@ latexmlscripts="$HOME/git/LaTeXML/blib/script"
 singularitydir="$HOME/latexml"
 printf '\\newcommand{\\thetitle}{Calculus}\n\\printincolor\n\\usethreeDgraphics\n\\renewcommand{\\monthYear}{June 2021}\n' > options.tex
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexml --destination=lab/moverMunder/moverMunder.xml --nocomments lab/moverMunder/moverMunder
+#singularity exec $singularitydir/latexml.sif $latexmlscripts/latexml --destination=lab/moverMunder/moverMunder.xml --nocomments lab/moverMunder/moverMunder
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.html --nocomments lab/moverMunder/moverMunder.xml
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.html lab/moverMunder/moverMunder.xml
 
-singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.xhtml --nocomments lab/moverMunder/moverMunder.xml
+singularity exec $singularitydir/latexml.sif $latexmlscripts/latexmlpost --destination=lab/moverMunder/moverMunder.xhtml lab/moverMunder/moverMunder.xml
 
 exit 0
 

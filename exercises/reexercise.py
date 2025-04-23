7141de51c6e6de40d6bde4de2fd8e4c50f88a033
Tim Prescott <teepeemm@gmail.com>
2025-04-23 13:59:11 -0500

    after Python changing exerciseset

--- exercises/reexercise.py
+++ exercises/reexercise.py
@@ -6,9 +6,9 @@ from glob import glob
 for exfilename in glob('*.tex'):
     with open(exfilename) as exfile:
         lines = exfile.read()
-    lines = re.sub(r'\begin{exerciseset}{In Exercises}', r'\exercisesetinstructions', lines)
-    if re.match(r'\begin{exerciseset}', lines):
+    lines = re.sub(r'\\begin{exerciseset}{In Exercises}', r'\\exercisesetinstructions', lines)
+    if re.match(r'\\begin{exerciseset}', lines):
         print(exfilename)
-    lines = re.sub(r'\end{exerciseset}', r'\exercisesetend', lines)
-    with open(exfilename) as exfile:
+    lines = re.sub(r'\\end{exerciseset}', r'\\exercisesetend', lines)
+    with open(exfilename, 'w') as exfile:
         exfile.write(lines)

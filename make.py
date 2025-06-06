4101f396c104eabb7f145ec1c98daacd33ea0d62
Tim Prescott <teepeemm@gmail.com>
2025-06-06 10:38:04 -0500

    fix options for larger lualatex memory

--- make.py
+++ make.py
@@ -478,7 +478,8 @@ def getcommandline(args):
         return getlatexmlepubcommandline()
     if args.standalonee:
         return getlatexmlepubcommandline('standalone','standaloneweb')
-    return [['latexmk','-lualatex','-main-memory=1000000','-hash-extra=1000000','Calculus'],
+    # the max-strings option for latexmk is not main-memory
+    return [['latexmk','-lualatex','-max-strings=1000000','-hash-extra=1000000','Calculus'],
         ['lualatex','--cnf-line="max_strings=1000000"','--cnf-line="hash_extra=1000000"','Calculus']]
 
 def minimizePdf(filename):
